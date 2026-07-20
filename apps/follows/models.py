9b85fbba95a30d2fc82f57d566a82dc18630487b
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-07-20 15:22:11 +0500

    Create models.py

---
+++ apps/follows/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['follower', 'following']
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['follower', 'following']),
+        ]
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
